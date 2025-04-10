1f3931dea06c5af6372e061d8cddd43c4041c5c9
himanshudestiny <himanshudestiny1997@gmail.com>
2025-04-10 10:25:28 +0530

    changes

--- rfp/src/pages/RFP/RFP.jsx
+++ rfp/src/pages/RFP/RFP.jsx
@@ -32,44 +32,44 @@ const RFP = (allRfpData) => {
             <Item {...allRfpData} />
           </Accordion.Body>
         </Accordion.Item>
-        <Accordion.Item eventKey="3"> 
+        <Accordion.Item eventKey="3">
           <Accordion.Header>BOQ</Accordion.Header>
-          <Accordion.Body> 
+          <Accordion.Body>
             <BOQ {...allRfpData} />
-          </Accordion.Body>  
+          </Accordion.Body>
         </Accordion.Item>
         <Accordion.Item eventKey="4">
           <Accordion.Header>BOQ Files</Accordion.Header>
           <Accordion.Body>
-            <BOQFiles {...allRfpData} />   
-          </Accordion.Body>
-        </Accordion.Item>   
-        <Accordion.Item eventKey="5">
-          <Accordion.Header>Vendor Details</Accordion.Header>
-          <Accordion.Body>   
-            <VendorDetails {...allRfpData} /> 
+            <BOQFiles {...allRfpData} />
           </Accordion.Body>
         </Accordion.Item>
-        <Accordion.Item eventKey="6"> 
+        <Accordion.Item eventKey="5">
+          <Accordion.Header>Vendor Details</Accordion.Header>
+          <Accordion.Body>
+            <VendorDetails {...allRfpData} />
+          </Accordion.Body>
+        </Accordion.Item>
+        <Accordion.Item eventKey="6">
           <Accordion.Header>Terms & Conditions</Accordion.Header>
           <Accordion.Body>
             {/* <TermsAndConditions {...allRfpData} /> */}
           </Accordion.Body>
         </Accordion.Item>
         <Accordion.Item eventKey="7">
-          <Accordion.Header>Negotiation Rounds</Accordion.Header> 
+          <Accordion.Header>Negotiation Rounds</Accordion.Header>
           <Accordion.Body>
             <NegotiationRounds {...allRfpData} />
           </Accordion.Body>
-        </Accordion.Item> 
+        </Accordion.Item>
         <Accordion.Item eventKey="8">
-          <Accordion.Header>Extnd RFP</Accordion.Header> 
+          <Accordion.Header>Extnd RFP</Accordion.Header>
           <Accordion.Body>
-            <ExtendRFP {...allRfpData} /> 
+            <ExtendRFP {...allRfpData} />
           </Accordion.Body>
         </Accordion.Item>
       </Accordion>
-    </> 
+    </>
   );
 };
 
