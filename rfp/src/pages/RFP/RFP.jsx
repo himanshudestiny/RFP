d963d1b68ba2344770223cf65fb827948164b1c7
himanshudestiny <himanshudestiny1997@gmail.com>
2025-04-14 10:15:37 +0530

    locations added

--- rfp/src/pages/RFP/RFP.jsx
+++ rfp/src/pages/RFP/RFP.jsx
@@ -62,7 +62,7 @@ const RFP = (allRfpData) => {
             <NegotiationRounds {...allRfpData} />
           </Accordion.Body>
         </Accordion.Item>
-        <Accordion.Item eventKey="8"> 
+        <Accordion.Item eventKey="8">  
           <Accordion.Header>Extnd RFP</Accordion.Header>
           <Accordion.Body>
             <ExtendRFP {...allRfpData} />
