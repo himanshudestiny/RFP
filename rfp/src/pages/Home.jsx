fe12db87f4b4657820ce9c2ff5e78e804d411272
himanshudestiny <himanshudestiny1997@gmail.com>
2025-04-21 12:06:52 +0530

    latest changes

--- rfp/src/pages/Home.jsx
+++ rfp/src/pages/Home.jsx
@@ -30,7 +30,7 @@ const Home = () => {
   const [totalRecords, setTotalRecords] = useState(0);
   const [totalPages, setTotalPages] = useState(0);
   const [fullscreen, setFullscreen] = useState(true);
-  const [show, setShow] = useState(false);
+  const [show, setShow] = useState(false); 
   const [allRfpData, setAllRfpData] = useState({});
   const [ showAddRfp, setShowAddRfp ] = useState(false);
 
