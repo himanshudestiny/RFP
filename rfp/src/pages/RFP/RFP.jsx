0083c0b928fad847f656267eb4892aa02108a23a
himanshudestiny <himanshudestiny1997@gmail.com>
2025-04-23 10:26:42 +0530

    latest changes

--- rfp/src/pages/RFP/RFP.jsx
+++ rfp/src/pages/RFP/RFP.jsx
@@ -10,19 +10,18 @@ import TermsAndConditions from "./TermsAndConditions";
 import NegotiationRounds from "./NegotiationRounds";
 import ExtendRFP from "./ExtendRFP";
 
-const RFP = ({allRfpData}) => {
-
+const RFP = ({ allRfpData }) => {
   return (
     <>
       <Accordion defaultActiveKey={["0"]} alwaysOpen>
         <Accordion.Item eventKey="0">
-          <Accordion.Header>RFP Details</Accordion.Header>    
+          <Accordion.Header>RFP Details</Accordion.Header>
           <Accordion.Body>
             <Header {...allRfpData} />
           </Accordion.Body>
         </Accordion.Item>
         <Accordion.Item eventKey="1">
-          <Accordion.Header>Location Details</Accordion.Header>   
+          <Accordion.Header>Location Details</Accordion.Header>
           <Accordion.Body>
             <Locations {...allRfpData} />
           </Accordion.Body>
@@ -36,40 +35,40 @@ const RFP = ({allRfpData}) => {
         <Accordion.Item eventKey="3">
           <Accordion.Header>BOQ</Accordion.Header>
           <Accordion.Body>
-             <BOQ {...allRfpData} /> 
-             </Accordion.Body>
+            <BOQ {...allRfpData} />
+          </Accordion.Body>
         </Accordion.Item>
         <Accordion.Item eventKey="4">
           <Accordion.Header>BOQ Files</Accordion.Header>
           <Accordion.Body>
-            <BOQFiles {...allRfpData} /> 
+            <BOQFiles {...allRfpData} />
           </Accordion.Body>
         </Accordion.Item>
-        <Accordion.Item eventKey="5"> 
+        <Accordion.Item eventKey="5">
           <Accordion.Header>Vendor Details</Accordion.Header>
           <Accordion.Body>
             <VendorDetails {...allRfpData} />
           </Accordion.Body>
         </Accordion.Item>
-        <Accordion.Item eventKey="6"> 
+        <Accordion.Item eventKey="6">
           <Accordion.Header>Terms & Conditions</Accordion.Header>
           <Accordion.Body>
-            <TermsAndConditions {...allRfpData} /> 
+            <TermsAndConditions {...allRfpData} />
           </Accordion.Body>
         </Accordion.Item>
         <Accordion.Item eventKey="7">
           <Accordion.Header>Negotiation Rounds</Accordion.Header>
           <Accordion.Body>
-            <NegotiationRounds {...allRfpData} /> 
+            <NegotiationRounds {...allRfpData} />
           </Accordion.Body>
-        </Accordion.Item> 
-        <Accordion.Item eventKey="8">  
+        </Accordion.Item>
+        <Accordion.Item eventKey="8">
           <Accordion.Header>Extnd RFP</Accordion.Header>
           <Accordion.Body>
             <ExtendRFP {...allRfpData} />
           </Accordion.Body>
         </Accordion.Item>
-      </Accordion> 
+      </Accordion>
     </>
   );
 };
