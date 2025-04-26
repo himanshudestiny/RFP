73cec8c95cddd1d50660ed26ace44f402f7ecd68
himanshudestiny <himanshudestiny1997@gmail.com>
2025-04-26 11:45:46 +0530

    changes

--- rfp/src/pages/RFP/RFP.jsx
+++ rfp/src/pages/RFP/RFP.jsx
@@ -27,7 +27,7 @@ const RFP = ({ allRfpData }) => {
           </Accordion.Body>
         </Accordion.Item>
         <Accordion.Item eventKey="2"> 
-          <Accordion.Header>Item</Accordion.Header>
+          <Accordion.Header>Item</Accordion.Header> 
           <Accordion.Body>
             <Item {...allRfpData} />
           </Accordion.Body>
