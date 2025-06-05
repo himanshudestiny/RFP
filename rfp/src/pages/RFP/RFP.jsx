7ed934b5c05950efbcbaf46ec44841bca58da090
himanshudestiny <himanshudestiny1997@gmail.com>
2025-06-05 16:58:15 +0530

    changes

--- rfp/src/pages/RFP/RFP.jsx
+++ rfp/src/pages/RFP/RFP.jsx
@@ -13,16 +13,16 @@ import ExtendRFP from "./ExtendRFP";
 const RFP = ({ allRfpData }) => {
   return (
     <>
-      <Accordion defaultActiveKey={["0"]} alwaysOpen> 
-        <Accordion.Item eventKey="0"> 
-          <Accordion.Header>RFP Details</Accordion.Header> 
-          <Accordion.Body> 
-            <Header {...allRfpData} /> 
-          </Accordion.Body> 
-        </Accordion.Item> 
-        <Accordion.Item eventKey="1"> 
-          <Accordion.Header>Location Details</Accordion.Header> 
-          <Accordion.Body> 
+      <Accordion defaultActiveKey={["0"]} alwaysOpen>
+        <Accordion.Item eventKey="0">
+          <Accordion.Header>RFP Details</Accordion.Header>
+          <Accordion.Body>
+            <Header {...allRfpData} />
+          </Accordion.Body>
+        </Accordion.Item>
+        <Accordion.Item eventKey="1">
+          <Accordion.Header>Location Details</Accordion.Header>
+          <Accordion.Body>
             <Locations {...allRfpData} />
           </Accordion.Body>
         </Accordion.Item>
